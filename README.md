a002d9d350e25da64329d916dc7035961e6686a8
Elio Rene Lopez <ElioRene.Lopez@md.de>
2021-02-22 09:47:45 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# github-api-python
+## github api python example
+
+Python 3.8.6
+
+Sample appliaction that retrieves repos and ids from github and show it to the screen.
+usage:
+
+´´´bash
+python main.py --user=<user> --token=<token>
+247069707   freenet-group/repo-info
+267538256   freenet-group/connect-redis
+267792284   freenet-group/architecture-decision-records
+267842702   freenet-group/connect-health
+267853992   freenet-group/connect-config
+...
+´´´
---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import argparse
+import requests
+
+def main():
+  base = "/orgs/freenet-group/repos?per_page=100"
+  server = "https://api.github.com"
+  url = server + base
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+      "-u",
+      "--user",
+      default="",
+      help="Specify the username to log into github")
+
+  parser.add_argument(
+      "-t",
+      "--token",
+      default="",
+      help="Specify the token to use github")
+
+  options = parser.parse_args()
+
+#API query to get a json answer
+#  r = requests.get(url, auth = (options.user, options.token))
+#  dump = r.json()
+  dump = requests.get(url, auth = (options.user, options.token)).json()
+
+#with this query you can see the structure fetched
+  print(json.dumps(dump[1], indent=4, sort_keys=True))
+
+#with this on you can iterate through first page answer
+  for index in range(0,len(dump)):
+    print(json.dumps(dump[index]['id']), " ", dump[index]['full_name'])
+
+#with this on you get a second poage answer and parse it
+  url = url + "&page=2"
+  dump = requests.get(url, auth = (options.user, options.token)).json()
+
+  for index in range(0,len(dump)):
+    print(json.dumps(dump[index]['id']), " ", dump[index]['full_name'])
+
+
+
+if __name__ == "__main__":
+    main()
